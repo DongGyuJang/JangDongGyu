79c5d44148681a23a32f79af148190ef424b6f31
DongGyuJang <118170454+DongGyuJang@users.noreply.github.com>
2022-11-17 17:58:37 +0900

    Add files via upload

---
+++ 개인소개페이지-app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://JangDongGyu:skyboy6612@cluster0.5jqpbnw.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+    return render_template('Main Page.html')
+
+
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name':name_receive,
+        'comment':comment_receive
+    }
+
+    db.homework.insert_one(doc)
+
+    return jsonify({'msg': '좋아'})
+
+
+@app.route("/homework", methods=["GET"])
+def web_good_get():
+    comment_list = list(db.homework.find({}, {'_id': False}))
+    return jsonify({'comments':comment_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
